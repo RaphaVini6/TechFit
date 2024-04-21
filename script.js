7590786c12a721dcc292881d18a9130bedcb2314
raphavini6 <raphaeluchoas21@gmail.com>
2024-04-21 00:27:16 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Adicionando um evento de clique ao botão "COMECE AGORA"
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.start-now').addEventListener('click', function() {
+        alert('Seu código JavaScript está funcionando!');
+    });
+});
