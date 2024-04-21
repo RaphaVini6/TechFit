0e4c6ad04e20f5eb9988d9aa0e4d839012addb19
raphavini6 <raphaeluchoas21@gmail.com>
2024-04-21 00:59:16 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-
 // Adicionando um evento de clique ao botão "COMECE AGORA"
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.start-now').addEventListener('click', function() {
         alert('Seu código JavaScript está funcionando!');
+    });
+
+    // Rolagem Suave para Links Internos
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 });
